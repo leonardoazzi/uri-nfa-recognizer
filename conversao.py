459a1f52d4616fa4cd9b80597f63c777f6cde7b9
Leonardo Azzi Martins <leoazzim@gmail.com>
2024-08-22 16:49:08 -0300

    Otimiza e refatora a conversao e reconhecimento (#6)

--- conversao.py
+++ conversao.py
@@ -35,7 +35,8 @@ def afn_para_afd(afn: AF) -> AF:
         estado_atual = estados_nao_processados.pop()
         estados_processados.add(estado_atual)
 
-        # Verifica se o estado atual contém um estado de aceitação do AFN
+        # Verifica se o estado atual contém um estado de aceitação do AFN,
+        # mesmo estados agrupados pela determinização em um frozenset
         if any(estado in afn.estados_aceita for estado in estado_atual):
             afd.estados_aceita.add(estado_atual)
 
@@ -50,11 +51,12 @@ def afn_para_afd(afn: AF) -> AF:
             novo_estado = frozenset(estados_atingidos)
 
             # Se o novo_estado não foi processado, adiciona à lista de estados não processados
-            if novo_estado not in estados_processados:
+            if novo_estado and novo_estado not in estados_processados:
                 estados_nao_processados.append(novo_estado)
-                afd.estados.add(novo_estado) # Adiciona o novo estado ao AFD
 
-            # Adiciona ao AFD a transição do estado atual para o novo estado
-            afd.transicoes[(estado_atual, simbolo)] = novo_estado
+            if novo_estado:
+                afd.estados.add(novo_estado) # Adiciona o novo estado ao AFD
+                # Adiciona ao AFD a transição do estado atual para o novo estado
+                afd.transicoes[(estado_atual, simbolo)] = novo_estado
 
     return afd
